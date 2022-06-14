7bab29bb7169b0e8c8b24a1320d05a69c3c034c0
Luciano Vallejo <luciano.m.vallejo@gmail.com>
2022-06-14 19:21:57 -0300

    Final terminado

--- js/index.js
+++ js/index.js
@@ -89,7 +89,6 @@ fetch('json/aceites.json')
     .then(response => response.json())
     .then(datos => {
         let productos = [...datos]
-        console.log(productos)
     })
 
 
@@ -111,45 +110,74 @@ function allProducts() {
         let productos = [...datos]
         let allProductos = document.getElementById('products')
         productos.map((product) => {
-            console.log(product)
-            allProductos.innerHTML = `
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Marca</th>
-                            <th scope="col">Especificacion</th>
-                            <th scope="col">Tipo</th>
-                            <th scope="col">Precio</th>
-                        </tr>
-                    </thead>
-                    <tbody>
+            allProductos.innerHTML += `
                         <tr>
                             <th scope="row" id="producto${product.id}">${product.id}</th>
                             <td>${product.marca}</td>
                             <td>${product.spec}</td>
                             <td>${product.tipo}</td>
                             <td>$${product.precio}</td>
+                            <td><button class="btn btn-primary" onclick='buy(${product.id})'>Comprar</button></td>
                         </tr>
-                    </tbody>
-                </table>
              `
         })
         
         
     })
 }
+
+let buyes = []
+const buy = (id) => {
+    fetch('json/aceites.json')
+    .then(response => response.json())
+    .then(datos => {
+        let productos = [...datos]
+        let filtered = productos.filter((element) => element.id == id)
+        buyes.push(filtered[0])
+    })
+    let productFiltered = JSON.stringify(buyes)
+    sessionStorage.setItem('compra', productFiltered)
+}
+function myCart() {
+    let gettingBuyer = JSON.parse(sessionStorage.getItem('compra'))
+    let readyForToBuy = document.getElementById('buyer')
+    if (readyForToBuy ?? '') {
+        gettingBuyer.forEach((product) => {
+            readyForToBuy.innerHTML += `
+                <tr>
+                    <th scope="row" id="producto${product.id}">${product.id}</th>
+                    <td>${product.marca}</td>
+                    <td>${product.spec}</td>
+                    <td>${product.tipo}</td>
+                    <td>$${product.precio}</td>
+                </tr>  
+            `
+        })
+    }
+}
+
+function endBuy() {
+    Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: 'Felicidades Compraste tus Productos',
+        showConfirmButton: false,
+        timer: 1500
+    })
+    let readyForToBuy = document.getElementById('buyer')
+    readyForToBuy.innerHTML = ''
+    sessionStorage.removeItem('compra')
+}
+
+
+
 function creatProductOil() {
     if (document.getElementById('marca').value != '' && document.getElementById('spec').value != '' && document.getElementById('price').value != '' && document.getElementById('stock').value != '') {
         let marca = document.getElementById('marca').value
         let spec = document.getElementById('spec').value
         let price = document.getElementById('price').value
         let stock = document.getElementById('stock').value
-        // newOilProduct = new AceiteProduct(marca, spec, price, stock)
         try {
-            // console.log(newOilProduct ?? 'Error')
-            // oilList.push(newOilProduct)
-            // ModalMsg('Producto Creado', {msg: 'El producto se agregado exitosamente'})
             alertMsg(
                 {
                     id: 'crear',
@@ -172,7 +200,6 @@ function creatProductOil() {
             
         }
     } else {
-        // ModalAlert('Error al Crear Producto', {msg: 'Para crear un producto debe completar todos los campos del mismo'})
         alertIssues({
             title: 'Error al Crear Producto' ,
             msg: 'Para crear un producto debe completar todos los campos del mismo'
@@ -186,36 +213,23 @@ function creatProductOil() {
 }
 
 function searchProductOil() {
+    let productos = []
     let searchMarca = document.getElementById('marcaSearch').value
     fetch('json/aceites.json')
         .then(response => response.json())
         .then(datos => {
-            let productos = [...datos]
+            productos = [...datos]
             if (searchMarca != '') {
-                console.log(productos.filter((element) => element.marca.includes(searchMarca)))
                 let filtered = productos.filter((element) => element.marca.includes(searchMarca))
                 let divProductos = document.getElementById('searchedProduct')
-                filtered.forEach((product, index) => {
-                    divProductos.innerHTML = `
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">Marca</th>
-                                    <th scope="col">Especificacion</th>
-                                    <th scope="col">Precio</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <th scope="row" id="producto${index}">${index}</th>
-                                    <td>${product.marca}</td>
-                                    <td>${product.spec}</td>
-                                    <td>$${product.precio}</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <button class="btn btn-primary" onclick='close()' id="close">Cerrar</button>
+                filtered.forEach((product) => {
+                    divProductos.innerHTML += `
+                        <tr>
+                            <th scope="row" id="producto${product.id}">${product.id}</th>
+                            <td>${product.marca}</td>
+                            <td>${product.spec}</td>
+                            <td>${product.tipo}</td>
+                        </tr>
                     `
                 })
             } else {
@@ -231,9 +245,6 @@ const close = () => {
 function deleteProductOil() {
     let deleteMarca = document.getElementById('marcaDelete').value
     deleteMarca != '' ? (
-        // oilList.splice(oilList.findIndex((element) => element.marca.includes(deleteMarca)), 1),
-        // ModalMsg('Producto Eliminado', {msg: 'Se elimino el Aceite: ' + deleteMarca}),
-        // console.log(oilList),
         alertMsg(
             {
                 id: 'eliminar',
@@ -309,67 +320,3 @@ const alertIssues = (error) => {
     })
 }
 
-function start() {
-    activeWhile = true
-    while (activeWhile) {
-        startInput = prompt('Que opcion desea elegir: \n 1 - Agregar Nuevo Producto Aceite \n 2 - Indicar cantidad de Aceites del inventario \n 3 - Buscar por marca \n 4 - Eliminar Aceites \n 0 - Salir')
-        if (isNaN(startInput) && startInput != '') {
-            console.log('Por favor elija un menu')
-        } else {
-            if (startInput == 1) {
-                creatOilProduct = true
-                while (creatOilProduct) {
-                    startInput = prompt('Desea agregar un Aceite nuevo? S o N')
-                    if (startInput != 'S' && startInput != 's' && startInput != 'N' && startInput != 'n') {
-                        console.log('Debe ingresar una opcion')
-                    } else {
-                        if (startInput === 'S' || startInput === 's') {
-                            newOilProduct = new AceiteProduct(
-                                prompt('Ingrese Marca del Aceite'),
-                                prompt('Ingrese que de especificacion posee'),
-                                parseFloat(prompt('Ingrese el precio')),
-                                parseInt(prompt('Declare el stock'))
-                            )
-                            oilList.push(newOilProduct)
-                        } else {
-                            creatOilProduct = false
-                            console.log(oilList)
-                        }
-                    }
-                }
-            } else {
-                if (startInput == 2) {
-                    console.log(oilList.length)
-                } else {
-                    if (startInput == 3) {
-                        let searchOil = prompt('Que marca de aceite desea buscar: ')
-                        if (searchOil == '') {
-                            console.log('Error al buscar este aceite')
-                        } else {
-                            console.log(oilList.filter((element) => element.marca.includes(searchOil)))
-                        }
-                    } else {
-                        if (startInput == 4) {
-                            let deleteOil = prompt('Que marca de aceite desea eliminar: ')
-                            if (deleteOil != '') {
-                                console.log(oilList)
-                                // let index = oilList.indexOf(deleteOil)                                
-                                // console.log(index)
-                                // console.log(oilList.findIndex((element) => element.instrumento == deleteOil))
-                                oilList.splice(oilList.findIndex((element) => element.marca.includes(deleteOil)), 1)
-                                console.log('Se elimino el Aceite: ' + deleteOil)
-                                console.log(oilList)
-                            } else {
-                                console.log('Error al buscar el aceite para borrar')
-                            }
-                        } else {
-                            if (startInput == 0) {
-                                activeWhile = false
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
